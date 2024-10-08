b9137395ab78c435568a8d17b836e73b6e2ed849
Melvin-Shalom <okshalommelvin@gmail.com>
2024-10-08 12:23:30 +0530

    Resume_Portfolio_02

--- js/scripts.js
+++ js/scripts.js
@@ -21,15 +21,17 @@
       }
     }
   });
-  // Closes responsive menu when a scroll trigger link is clicked
+
   $(".js-scroll-trigger").click(function() {
     $(".navbar-collapse").collapse("hide");
   });
-  // Activate scrollspy to add active class to navbar items on scroll
+
   $("body").scrollspy({
     target: "#sideNav",
   });
-})(jQuery); // End of use strict
+})(jQuery);
+
+
 //dark mode
 function make_me_dark() {
   var element = document.body;
